85e415bfb6e90bb7b8483a36b7452f214a6333d5
William Putra <william.putra@viseo.com>
2020-12-01 13:20:08 +0700

    12 December 2020

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -6,7 +6,7 @@ const Exercise = props => (
     <tr>
         <td>{props.exercise.username}</td>
         <td>{props.exercise.description}</td>
-        <td>{props.exercise.duratiom}</td>
+        <td>{props.exercise.duration}</td>
         <td>{props.exercise.date.substring(0,10)}</td>
         <td>
             <Link to={"/edit/"+props.exercise._id}>edit</Link> | <a href="#" onClick={() => {props.deleteExercise(props.exercise._id)}}>delete</a>
